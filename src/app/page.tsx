5e1f8693c4c9672d376ca9d08ed5f4fbdf1fe611
Neil Morrison <neilmorrison@neils-mbp.home>
2024-05-15 17:41:41 -0400

    linting...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,8 @@
-import { useEffect, useMemo, useState } from "react";
 import ListingCard from "@/components/ListingCard";
 import MainPageFilters from "@/components/MainPageFilters";
 import { fetchFilteredListings } from "./actions";
 import { LISTING_STATUS } from "@prisma/client";
 import { SListingWithAddress } from "@/lib/listings";
-
-type HomeProps = {
-  params?: {
-    num?: string;
-  };
-  searchParams?: {
-    status?: LISTING_STATUS;
-  };
-};
 
 export default async function Home({ searchParams }: { searchParams: { [key: string]: string | string[] | LISTING_STATUS } }) {
   const listings = await fetchFilteredListings({ status: searchParams.status as LISTING_STATUS || LISTING_STATUS.IMMEDIATE })
@@ -21,7 +11,7 @@ export default async function Home({ searchParams }: { searchParams: { [key: str
     <main className="mx-24 my-12">
       <MainPageFilters />
       <section className="grid grid-cols-4 gap-6 mt-4">
-        {listings.map((listing) => {
+        {listings.map((listing: SListingWithAddress) => {
           return <ListingCard key={listing.id} listing={listing} />;
         })}
       </section>
