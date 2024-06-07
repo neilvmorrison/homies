755b3da966f18bda603181ec008ba9175958c1ca
Neil Morrison <neilmorrison@neils-mbp.home>
2024-06-07 11:19:33 -0400

    added new routes and stylign

--- src/app/learn-more/layout.tsx
+++ src/app/learn-more/layout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react'
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return <div className="max-w-xl mx-auto">{children}</div>
+  return <div className="max-w-[55vw] ml-[80px] mb-[200px]">{children}</div>
 }
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,7 @@
+export default function Signup() {
+  return (
+    <div>
+      <h1>SIGN UP HERE</h1>
+    </div>
+  )
+}
